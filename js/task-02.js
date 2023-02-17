45100829f4a899e03a03a25cfae2d13cf50ac4b4
karolinakiljanska <karolinakiljanska112@gmail.com>
2023-02-17 21:24:42 +0100

    zadanie nr 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const ingredientsList = document.querySelector("#ingredients");
+
+const items = ingredients.map((ingredient) => {
+  const listItem = document.createElement("li");
+  listItem.classList.add("item");
+  listItem.textContent = ingredient;
+
+  return listItem;
+});
+
+ingredientsList.append(...items);
