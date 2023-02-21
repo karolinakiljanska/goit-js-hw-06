90cb3c41c4965e38ec64a037c17639ba8c4b94f8
karolinakiljanska <karolinakiljanska112@gmail.com>
2023-02-21 18:28:44 +0100

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+const changeBtn = document.querySelector(".change-color");
+const body = document.querySelector("body");
+const colorChange = document.querySelector(".color");
+changeBtn.addEventListener("click", changeColor);
+
+function changeColor() {
+  let color = getRandomHexColor();
+  body.style.background = color;
+  colorChange.textContent = color;
 }
